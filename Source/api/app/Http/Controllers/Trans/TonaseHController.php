62f96c3517cdeef8fd41ee6f1d2c7d34ef4373a3
muhamadfarkhan <muhamad.farkhan24@gmail.com>
2021-06-02 07:34:56 +0700

    Progress Tonase Header, detail

--- Source/api/app/Http/Controllers/Trans/TonaseHController.php
+++ Source/api/app/Http/Controllers/Trans/TonaseHController.php
@@ -46,7 +46,11 @@ class TonaseHController extends Controller
             $tonase['sum_ekor'] = $tonaseDetail->sum('ekor');
             $tonase['sum_kilo'] = $tonaseDetail->sum('kilogram');
 
-            $tonaseDetail['kilogram'] = $tonaseDetail->kilogram.' Kg';
+            foreach($tonaseDetail as $detail){
+                $tonaseDetail['kilogram'] = 'test';
+            }
+
+            // $tonaseDetail['kilogram'] = $tonaseDetail->kilogram.' Kg';
 
             return response()->json(['tonase_header' => $tonase, 'tonase_details' => $tonaseDetail], 200);
 
